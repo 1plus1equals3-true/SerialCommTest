37051babffc444d42989f086e31f78ded77900fa
won <rkdfksms1@gmail.com>
2026-02-08 02:24:58 +0900

    Serial

---
+++ Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace SerialCommTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.comboCOM = new System.Windows.Forms.ComboBox();
+            this.richLog = new System.Windows.Forms.RichTextBox();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.txtSend = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(139, 12);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(75, 23);
+            this.btnConnect.TabIndex = 0;
+            this.btnConnect.Text = "연결";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // comboCOM
+            // 
+            this.comboCOM.FormattingEnabled = true;
+            this.comboCOM.Location = new System.Drawing.Point(12, 12);
+            this.comboCOM.Name = "comboCOM";
+            this.comboCOM.Size = new System.Drawing.Size(121, 20);
+            this.comboCOM.TabIndex = 1;
+            // 
+            // richLog
+            // 
+            this.richLog.Location = new System.Drawing.Point(220, 14);
+            this.richLog.Name = "richLog";
+            this.richLog.Size = new System.Drawing.Size(541, 381);
+            this.richLog.TabIndex = 2;
+            this.richLog.Text = "";
+            // 
+            // txtSend
+            // 
+            this.txtSend.Location = new System.Drawing.Point(12, 106);
+            this.txtSend.Name = "txtSend";
+            this.txtSend.Size = new System.Drawing.Size(202, 21);
+            this.txtSend.TabIndex = 3;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(12, 133);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(202, 23);
+            this.btnSend.TabIndex = 4;
+            this.btnSend.Text = "보내기";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.txtSend);
+            this.Controls.Add(this.richLog);
+            this.Controls.Add(this.comboCOM);
+            this.Controls.Add(this.btnConnect);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.ComboBox comboCOM;
+        private System.Windows.Forms.RichTextBox richLog;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.TextBox txtSend;
+        private System.Windows.Forms.Button btnSend;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.IO.Ports;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SerialCommTest
+{
+    public partial class Form1 : Form
+    {
+        const char STX = '\x02';
+        const char ETX = '\x03';
+        private StringBuilder rxBuffer = new StringBuilder();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] ports = SerialPort.GetPortNames();
+
+            comboCOM.Items.AddRange(ports);
+
+            if (comboCOM.Items.Count > 0)
+            {
+                comboCOM.SelectedIndex = 0;
+            }
+
+            serialPort1.DataReceived += new SerialDataReceivedEventHandler(serialPort1_DataReceived);
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (serialPort1.IsOpen)
+                {
+                    serialPort1.Close();
+                    Log("연결이 해제되었습니다.");
+                    btnConnect.Text = "연결";
+                    comboCOM.Enabled = true;
+                }
+                else
+                {
+                    if(comboCOM.Text == "")
+                    {
+                        Log("포트를 선택해주세요.");
+                        return;
+                    }
+                    serialPort1.PortName = comboCOM.Text;
+                    serialPort1.BaudRate = 9600;
+                    serialPort1.Encoding = Encoding.UTF8;
+
+                    serialPort1.Open();
+
+                    Log($"{comboCOM.Text}포트에 연결되었습니다");
+                    btnConnect.Text = "해제";
+                    comboCOM.Enabled = false;
+                }
+            }
+            catch (Exception ex) { Log($"에러 발생: {ex.Message}"); }
+        }
+
+        private void Log(string msg)
+        {
+            string currentTime = DateTime.Now.ToString("HH:mm:ss");
+            richLog.AppendText($"[{currentTime}] {msg}\r\n");
+            richLog.ScrollToCaret();
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            if (!serialPort1.IsOpen) 
+            { Log("포트가 연결되지 않았습니다."); return; }
+
+            try
+            {
+                string data = txtSend.Text;
+                string packet = $"{STX}{data}{ETX}";
+                serialPort1.Write(packet);
+                //serialPort1.WriteLine(data);
+                Log($"[TX] 보냄 : {data} (STX/ETX)");
+                txtSend.Clear();
+            }
+            catch (Exception ex)
+            {
+                Log($"전송 실패 : {ex.Message}");
+            }
+            
+        }
+
+        private void serialPort1_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            try
+            {
+                string recvData = serialPort1.ReadExisting();
+                rxBuffer.Append(recvData);
+
+                while (true)
+                {
+                    string currentBuffer = rxBuffer.ToString();
+                    int stxIndex = currentBuffer.IndexOf(STX);
+                    int etxIndex = currentBuffer.IndexOf(ETX);
+
+                    if (stxIndex != -1 && etxIndex != -1 && stxIndex < etxIndex)
+                    {
+                        string msg = currentBuffer.Substring(stxIndex + 1, etxIndex - stxIndex - 1);
+
+                        this.Invoke(new Action(() =>
+                        {
+                            Log($"[RX] 파싱 : {msg}");
+                        }));
+
+                        rxBuffer.Remove(0, etxIndex + 1);
+                    }
+                    else if (etxIndex != -1 && (stxIndex == -1 || etxIndex < stxIndex))
+                    {
+                        rxBuffer.Remove(0, etxIndex + 1);
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                if (!this.IsDisposed)
+                {
+                    this.Invoke(new Action(() => Log($"수신 실패 : {ex.Message}")));
+                }
+            }
+        }
+    }
+}
